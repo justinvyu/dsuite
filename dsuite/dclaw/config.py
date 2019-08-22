f4386e1a7f387ca1ae020faad22fe419f631c092
Henry Zhu <henryzhu@berkeley.edu>
2019-08-22 16:36:30 -0700

    Fix quat config
    Change-Id: I1dd334cd7932c1327d162998d9b45d5d240d48eb

--- dsuite/dclaw/config.py
+++ dsuite/dclaw/config.py
@@ -84,19 +84,10 @@ _FREE_OBJECT_QUAT_SIM_CONFIG = {
                 (-0.15, 0.15),     # restrict x
                 (-0.15, 0.15),     # restrict y
                 (0, 0.25),          # unrestricted z
-                (-np.pi, np.pi), # unrestricted object orientation
-                (-np.pi, np.pi),
-                (-np.pi, np.pi)],
-            # Lift, Flip
-            # 'qpos_indices': range(-7, 0),  # w/ free joint
-            # 'qpos_range': [
-            #     (-0.15, 0.15),     # restrict x
-            #     (-0.15, 0.15),     # restrict y
-            #     (0, 0.25),          # unrestricted z
-            #     (0, 1), # avoid double cover
-            #     (-1, 1),
-            #     (-1, 1),
-            #     (-1, 1)],
+                (0, 1), # avoid double cover
+                (-1, 1),
+                (-1, 1),
+                (-1, 1)],
             'sim_observation_noise': 0,
         },
         'guide': {},  # The guide group is a no-op in simulation.
